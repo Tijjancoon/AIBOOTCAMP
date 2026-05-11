92267c194d014bad608fbe66ed8111f8a775c73f
Tijjancoon <104052702+Tijjancoon@users.noreply.github.com>
2026-05-11 20:51:37 +0300

    Add files via upload

---
+++ prac2.py
@@ -0,0 +1,18 @@
+#Define variables of different data types
+integer_var = 10
+float_var = 3.14
+string_var = "AI"
+list_var = [1, 2, 3]
+tuple_var = (4, 5, 6)
+dict_var = {"name": "Tijjan", "role": "It student"}
+bool_var = True
+
+#print and manipulate variables
+print("Integer:", integer_var)
+print("float:", float_var)
+print("string:", string_var + " Bootcamp")
+list_var.append(4)
+print("list:", list_var)
+print("tuple:", tuple_var)
+print("dictionary:", dict_var["name"])
+print("boolean:", bool_var)
