f05edcb4161d1fb2f78be3d0ceba25e43d81e5fa
Tijjancoon <104052702+Tijjancoon@users.noreply.github.com>
2026-05-12 23:34:16 +0300

    Add files via upload

---
+++ prac2_1.py
@@ -0,0 +1,16 @@
+ #Example 1: Checking a condition
+ num = -12
+ if num > 0:
+     print("Positive")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative")
+    
+#Example 2: Nested conditions
+age = 20
+if age > 18:
+    if age < 30:
+        print("Young Adult")
+else:
+    print("Adult")
